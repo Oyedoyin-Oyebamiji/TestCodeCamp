597bacbd9461c0a1fed19b60ae83fedf9dbb31b9
Oyedoyin-Oyebamiji/TestCodeCamp <oyedoyinoyebamiji@gmail.com>
2022-07-04 12:09:18 +0100

    Firstjavaproject

---
+++ Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world! this is my first java project! I am so excited to be writing this java code");
+    }
+}
